fc4c4646108e31b6af7903faa1a2b4012b7dd0b7
Adharsraj <138186421+Adharsraj@users.noreply.github.com>
2024-06-25 15:22:57 +0530

    new

---
+++ apps/user/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div className="text-blue-00">
+fd
+<Button variant={"destructive"}>df</Button>
+
+   </div>
+  );
+}
