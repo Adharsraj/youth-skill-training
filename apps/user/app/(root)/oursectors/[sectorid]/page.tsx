199ab96184ea03171ad5145efca4455ca4ebf4e2
Adharsraj <138186421+Adharsraj@users.noreply.github.com>
2024-07-04 21:27:03 +0530

    new

--- apps/user/app/(root)/oursectors/[sectorid]/page.tsx
+++ apps/user/app/(root)/oursectors/[sectorid]/page.tsx
@@ -186,7 +186,7 @@ const ElectronicsPage: React.FC<ElectronicsPageProps> = ({ params }) => {
   const p = params?.sectorid
 
   const sector = ourSectorDatas.find(sector =>
-    p.replace(/%20/g,' ').toLowerCase() === sector.title.toLowerCase()
+    p.replace(/%20/g, ' ').toLowerCase() === sector.title.toLowerCase()
   );
 
 
@@ -195,7 +195,35 @@ const ElectronicsPage: React.FC<ElectronicsPageProps> = ({ params }) => {
     <div>
       {sector ? (
         <>
-         <div className="relative md:h-[280px] h-[180px] w-full">
+          <div className="relative md:h-[280px] h-[180px] w-full">
+            <Image
+              src={sector.img}
+              alt="Section Image"
+              className="h-full object-cover"
+              layout="fill"
+            />
+            <div className="absolute inset-0 flex items-center justify-center text-white">
+              <div className="text-center">
+                <h2 className="lg:text-5xl md:text-4xl text-2xl font-bold">{sector.title}</h2>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+          <div className="w-full px-0">
+      {/* sec 1 */}
+         <section className="my-8 px-0">
+             {/* <div className="relative md:h-[280px] h-[180px] w-full">
                <Image
                  src={sector.img}
                  alt="Section Image"
@@ -204,15 +232,123 @@ const ElectronicsPage: React.FC<ElectronicsPageProps> = ({ params }) => {
                />
                <div className="absolute inset-0 flex items-center justify-center text-white">
                  <div className="text-center">
-                   <h2 className="lg:text-5xl md:text-4xl text-2xl font-bold">{sector.title}</h2>
+                   <h2 className="lg:text-5xl md:text-4xl text-2xl font-bold">{sector.desc}</h2>
                  </div>
                </div>
+             </div> */}
+             {/* sec 2 */}
+             <div className="flex flex-col-reverse md:flex-row items-center h-auto md:h-[300px] justify-center w-full px-0">
+               <div className="relative w-full md:w-1/3 h-[200px] md:h-full mb-4 md:mb-0 md:mr-8">
+                 <Image
+                   src={sector.s2Img}
+                   alt="Section Image"
+                   className="w-full h-full object-cover"
+                   layout="fill"
+                 />
+               </div>
+               <div className="w-full md:w-1/2">
+                 <h2 className="lg:text-3xl text-xl text-center lg:text-left font-bold text-red-600">{sector.s2heading}</h2>
+                 <p className="mt-4 md:text-md text-xs lg:text-lg text-center md:text-left">{sector.s2paragraph}</p>
+               </div>
              </div>
-         
+           {/* {section.section === 3 && (
+             <div className="flex flex-col md:flex-row items-center h-auto md:h-[380px] bg-primary-blue w-full px-0">
+               <div className="relative w-full md:w-1/2 overflow-hidden mb-4 md:mb-0">
+                 <div className="relative w-full h-full">
+                   <div
+                     className="flex transition-transform duration-500"
+                     style={{
+                       transform: `translateX(-${currentSlide * 100}%)`,
+                     }}
+                   >
+                     {section.slider && section.slider.map((slide, slideIndex) => (
+                       <div key={slideIndex} className="flex-none w-full flex-shrink-0 px-0 py-2">
+                         <h3 className="lg:text-xl md:text-xl text-xs font-semibold text-white text-center">{section.sliderHeading}</h3>
+                         <h3 className="lg:text-xl md:text-xl text-xs font-semibold text-white text-center">{slide.heading}</h3>
+                         <p className="mt-2 text-white md:text-xs lg:text-md text-xs text-center w-[180px] md:w-[300px] mx-auto">{slide.description}</p>
+                       </div>
+                     ))}
+                   </div>
+                   <button
+                     className="absolute top-1/2 left-0 transform -translate-y-1/2 ml-[30px]  md:ml-[20px] lg:ml-[160px] text-white px-2 py-1 z-10"
+                     onClick={prevSlide}
+                   >
+                     <svg
+                       xmlns="http:www.w3.org/2000/svg"
+                       className="h-6 w-6"
+                       fill="none"
+                       viewBox="0 0 24 24"
+                       stroke="currentColor"
+                     >
+                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                     </svg>
+                   </button>
+                   <button
+                     className="absolute top-1/2 right-0 transform -translate-y-1/2  mr-[30px]  md:mr-[20px] lg:mr-[160px] text-white px-2 py-1 z-10"
+                     onClick={nextSlide}
+                   >
+                     <svg
+                       xmlns="http:www.w3.org/2000/svg"
+                       className="h-6 w-6"
+                       fill="none"
+                       viewBox="0 0 24 24"
+                       stroke="currentColor"
+                     >
+                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                     </svg>
+                   </button>
+                 </div>
+               </div>
+               <div className="relative w-full md:w-1/3 h-[200px] md:h-[280px] ml-0 md:ml-8">
+                 <Image
+                   src={section.image}
+                   alt="Section Image"
+                   className="lg:w-full lg:h-full w-[300px] h-[300px] object-cover"
+                   layout="fill"
+                 />
+               </div>
+             </div>
+           )} */}
+             <div className="flex flex-col md:flex-row items-center h-auto md:h-[280px] justify-center w-full px-0">
+               <div className="w-full md:w-1/2">
+                 <h2 className="lg:text-3xl font-bold md:text-xl text-lg text-red-600 lg:text-left text-center">{sector.s4heading}</h2>
+                 <p className="mt-4 md:text-md lg:text-lg text-xs text-center lg:text-left">{sector.s4paragraph}</p>
+               </div>
+               <div className="relative w-full md:w-1/3 h-[200px] md:h-full ml-0 md:ml-8 mt-4 md:mt-0">
+                 <Image
+                   src={sector.s4Img}
+                   alt="Section Image"
+                   className="w-full h-full object-cover"
+                   layout="fill"
+                 />
+               </div>
+             </div>
+         </section>
+     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </>
       ) : (
-        <p>No sector found with the title {params.sectorid.replace(/%20/g,' ').toLowerCase()}</p>
+        <p>No sector found with the title {params.sectorid.replace(/%20/g, ' ').toLowerCase()}</p>
       )}
+
+
+
+
+
     </div>
   );
 }
--- apps/user/constants/ourSectorDatas.ts
+++ apps/user/constants/ourSectorDatas.ts
@@ -12,42 +12,137 @@ import st5 from '../public/marquee/marqueeSectors/5.svg'
 import st6 from '../public/marquee/marqueeSectors/6.svg'
 
 
+import sectionOneImage from '../../user/public/electronics/electronics.svg';
+import sectionTwoImage from '../../user/public/electronics/e1.svg';
+import sectionThreeImage from '../../user/public/electronics/e2.svg';
+import sectionFourImage from '../../user/public/electronics/e3.svg';
 
+import beat1 from '../../user/public/beauty/beauty1.svg';
+import beat2 from '../../user/public/beauty/b1.svg';
+import beat3 from '../../user/public/beauty/b2.svg';
+import beat4 from '../../user/public/beauty/b3.svg';
+
+import elec1 from '../../user/public/electricals/electricals.svg';
+import elec2 from '../../user/public/electricals/el1.svg';
+import elec3 from '../../user/public/electricals/el2.svg';
+import elec4 from '../../user/public/electricals/el3.svg';
+
+import information1 from '../../user/public/IT/Top1.svg';
+import information2 from '../../user/public/IT/top2.svg';
+import information3 from '../../user/public/IT/top3.svg';
+import information4 from '../../user/public/IT/top4.svg';
+
+import retail1 from '../../user/public/retails/retail.svg';
+import retail2 from '../../user/public/retails/rt1.svg';
+import retail3 from '../../user/public/retails/rt2.svg';
+import retail4 from '../../user/public/retails/rt3.svg';
+
+import health1 from '../../user/public/health/health.svg';
+import health2 from '../../user/public/health/hl1.svg';
+import health3 from '../../user/public/health/hl2.svg';
+import health4 from '../../user/public/health/hl3.svg';
 
 
 
 
 export const ourSectorDatas = [
     {
-      img: st1,
-      title:"Electronics",
-      desc:"Under the Grant-in-Aided Scheme, Government of India invites applications for various Diploma Courses recognized by the Government "
+        img: st1,
+        title: "Electronics",
+        desc: "Under the Grant-in-Aided Scheme, Government of India invites applications for various Diploma Courses recognized by the Government ",
+        slider: [
+            {
+                heading: "Fundamentals of Electronics",
+                description: " Learn the basics of electronic components, circuits, and systems, essential for understanding modern technology."
+            },
+            {
+                heading: "Slide 2 Heading",
+                description: "Slide 2 Description"
+            },
+            {
+                heading: "Slide 3 Heading",
+                description: "Slide 3 Description"
+            },
+            {
+                heading: "Slide 4 Heading",
+                description: "Slide 4 Description"
+            }
+        ],
+        sliderHeading: "What we Teach",
+        s2Img: sectionTwoImage,
+        s2heading: "Electronics",
+        s2paragraph: "Electronics encompasses the study and application of devices and systems that utilize the flow of electrons to process information or perform tasks. This field is crucial in modern technology, ranging from consumer electronics like smartphones and computers to complex systems such as medical equipment and satellites. Understanding electronics involves knowledge of circuitry, semiconductor devices, digital systems, and more, making it fundamental to various industries and a key area of focus for career training in technology and engineering",
+        s3Img: sectionThreeImage,
+        s4Img: sectionFourImage,
+        s4heading: "Innovation and Problem-Solving",
+        s4paragraph: "Develop practical skills in understanding and working with electronic components, circuits, and systems, essential for a wide range of technical roles. Electronics training fosters creativity and problem-solving abilities, preparing individuals to innovate and adapt to technological advancements in various industries.Electronics expertise is highly sought after in sectors like telecommunications, consumer electronics, automotive, and renewable energy, offering diverse career opportunities and pathways for specialization."
     },
     {
-      img: st2,
-      title:"Information Technology",
-      desc:"Information Technology (IT) sector includes roles such as software developers, cybersecurity experts, and IT support specialists, crucial for managing and innovating technology systems."
+        img: st2,
+        title: "Information Technology",
+        desc: "Information Technology (IT) sector includes roles such as software developers, cybersecurity experts, and IT support specialists, crucial for managing and innovating technology systems."
     },
     {
-      img: st3,
-      title:"Beauty",
-      desc:"Beauty sector offers diverse roles like cosmetologists, estheticians, makeup artists, and salon managers, focusing on skincare, haircare, and wellness. "
+        img: st3,
+        title: "Beauty",
+        desc: "Beauty sector offers diverse roles like cosmetologists, estheticians, makeup artists, and salon managers, focusing on skincare, haircare, and wellness. "
     },
     {
-      img: st4,
-      title:"Electricals",
-      desc:"The electrical sector includes roles such as electrical engineers, renewable energy specialists, and power systems technicians, focusing on electrical infrastructure, maintenance, and innovation."
+        img: st4,
+        title: "Electricals",
+        desc: "The electrical sector includes roles such as electrical engineers, renewable energy specialists, and power systems technicians, focusing on electrical infrastructure, maintenance, and innovation."
     },
     {
-      img: st5,
-      title:"Retail",
-      desc:"The retail sector offers jobs such as sales associates, store managers, and customer service representatives, centered around selling products and services directly to consumers both in-store and online."
+        img: st5,
+        title: "Retail",
+        desc: "The retail sector offers jobs such as sales associates, store managers, and customer service representatives, centered around selling products and services directly to consumers both in-store and online."
     },
     {
-      img: st6,
-      title:"Health Care",
-      desc:"Healthcare sector offers roles like doctors, nurses, pharmacists, and medical technicians, focusing on patient care, treatment, and wellness."
+        img: st6,
+        title: "Health Care",
+        desc: "Healthcare sector offers roles like doctors, nurses, pharmacists, and medical technicians, focusing on patient care, treatment, and wellness."
     },
-   
-  ];
 
+];
+
+
+//   {
+//     section: 1,
+//     image: sectionOneImage,
+//     text: "Electronics"
+// },
+// {
+//     section: 2,
+//     image: sectionTwoImage,
+//     heading: "Electronics",
+//     paragraph: "Electronics encompasses the study and application of devices and systems that utilize the flow of electrons to process information or perform tasks. This field is crucial in modern technology, ranging from consumer electronics like smartphones and computers to complex systems such as medical equipment and satellites. Understanding electronics involves knowledge of circuitry, semiconductor devices, digital systems, and more, making it fundamental to various industries and a key area of focus for career training in technology and engineering"
+// },
+// {
+//     section: 3,
+//     image: sectionThreeImage,
+//     sliderHeading: "What we Teach",
+// slider: [
+//     {
+//         heading: "Fundamentals of Electronics",
+//         description: " Learn the basics of electronic components, circuits, and systems, essential for understanding modern technology."
+//     },
+//     {
+//         heading: "Slide 2 Heading",
+//         description: "Slide 2 Description"
+//     },
+//     {
+//         heading: "Slide 3 Heading",
+//         description: "Slide 3 Description"
+//     },
+//     {
+//         heading: "Slide 4 Heading",
+//         description: "Slide 4 Description"
+//     }
+// ]
+// },
+// {
+//     section: 4,
+//     image: sectionFourImage,
+//     heading: "Innovation and Problem-Solving",
+//     description: "Develop practical skills in understanding and working with electronic components, circuits, and systems, essential for a wide range of technical roles. Electronics training fosters creativity and problem-solving abilities, preparing individuals to innovate and adapt to technological advancements in various industries.Electronics expertise is highly sought after in sectors like telecommunications, consumer electronics, automotive, and renewable energy, offering diverse career opportunities and pathways for specialization."
+// }
